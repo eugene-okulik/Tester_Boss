bbe21e14d196af562c796bb73fa2e1b1fe4301b8
Mi <jiraoop69@gmail.com>
2026-02-16 18:25:26 +0300

    добавлено 3 домашнее задание

---
+++ homework/Ivan_Demidov/Homework_3/homework1.py
@@ -0,0 +1,10 @@
+num1 = 50
+num2 = 10
+
+sum1 = num1 + num2
+difference = num1 - num2
+multiplication = num1 * num2
+
+print(sum1)
+print(difference)
+print(multiplication)
---
+++ homework/Ivan_Demidov/Homework_3/homework3.py
@@ -0,0 +1,10 @@
+import math
+
+a = float(input("Введите первый катет: "))
+b = float(input("Введите второй катет: "))
+
+hypotenuse = math.sqrt(a**2 + b**2)
+area = (a * b) / 2
+
+print(f"Гипотенуза: {hypotenuse:.2f}")
+print(f"Площадь: {area:.2f}")
