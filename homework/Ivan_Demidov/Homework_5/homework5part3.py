84bfdc66b21ae1778c4c0ed87568f6e7ef772a6e
Mi <jiraoop69@gmail.com>
2026-02-24 18:46:10 +0300

    Homework_5 done

---
+++ homework/Ivan_Demidov/Homework_5/homework5part3.py
@@ -0,0 +1,7 @@
+students = ["Ivanov", "Petrov", "Sidorov"]
+subjects = ["math", "biology", "geography"]
+
+print(
+    f"Students {', '.join(students)} study these "
+    f"subjects: {', '.join(subjects)}"
+)
