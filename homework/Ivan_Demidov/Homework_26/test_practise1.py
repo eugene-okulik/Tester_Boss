729b79bee818bcc2c9b22e15724095e873499a4c
Mi <jiraoop69@gmail.com>
2026-08-07 18:54:28 +0300

    Fix Homework 26 tests

--- homework/Ivan_Demidov/Homework_26/test_practise1.py
+++ homework/Ivan_Demidov/Homework_26/test_practise1.py
@@ -15,7 +15,7 @@ def driver():
     chrome_driver.quit()
 
 
-def test__shop_1(driver):
+def test_shop_1(driver):
     driver.get("http://testshop.qa-practice.com/")
 
     link = driver.find_element(By.CLASS_NAME, "oe_product_image_link")
@@ -27,20 +27,43 @@ def test__shop_1(driver):
     driver.switch_to.window(driver.window_handles[1])
 
     add_button = WebDriverWait(driver, 10).until(
-        EC.visibility_of_element_located((By.ID, "add_to_cart"))
+        EC.element_to_be_clickable((By.ID, "add_to_cart"))
     )
 
-    driver.execute_script("arguments[0].click();", add_button)
+    add_button.click()
 
-    WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.CLASS_NAME, "btn-secondary"))
-    ).click()
+    continue_shopping = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.CLASS_NAME, "btn-secondary"))
+    )
+
+    assert "Customizable Desk" in driver.page_source
+
+    continue_shopping.click()
 
     driver.close()
 
     driver.switch_to.window(driver.window_handles[0])
 
     driver.refresh()
+
+    WebDriverWait(driver, 10).until(
+        lambda driver: driver.execute_script("return document.readyState") == "complete"
+    )
+
+    cart = WebDriverWait(driver, 10).until(
+        lambda driver: next(
+            (
+                element
+                for element in driver.find_elements(By.XPATH, '//a[@href="/shop/cart"]')
+                if element.is_displayed() and element.is_enabled()
+            ),
+            False,
+        )
+    )
+
+    cart.click()
+
+    WebDriverWait(driver, 10).until(EC.url_contains("/shop/cart"))
 
     assert "Customizable Desk" in driver.page_source
 
